a0ac2e437b810ecb4138b1ba0e52c4fc95a06897
bilykhbar <nabilikhbar21@gmail.com>
2025-10-02 15:26:12 +0700

    penamahan payment

---
+++ jajalasynchronousJS/asynchronous.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+    document.body.style.backgroundColor = "red";
+}, 1000);
+
+setTimeout(function(){
+    document.body.style.backgroundColor = "yellow";
+}, 2000);
+
+setTimeout(function(){
+    document.body.style.backgroundColor = "green";
+}, 3000);
